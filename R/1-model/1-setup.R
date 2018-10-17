529e36f6f82ae745adc4a98fe302cf3e76d81e80
Peter M C Harrison <pmc.harrison@gmail.com>
2018-10-17 12:39:10 +0100

    Renaming setup file for model analysis

--- R/1-model/1-setup.R
+++ R/1-model/1-setup.R
@@ -3,5 +3,9 @@ library(PPMdecay)
 library(changepoint)
 library(zeallot)
 
+loadNamespace("memoise")
+
+if (!dir.exists("cache")) dir.create("cache")
+
 for (f in list.files("R/1-model/functions/", full.names = TRUE, pattern = "\\.R$"))
   source(f)
