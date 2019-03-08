eca3f0506731f99556ebd0aec213835ff5220f76
Peter Harrison <pmc.harrison@gmail.com>
2019-03-08 10:42:00 +0000

    Updating PPM code

--- R/1-model/1-model.R
+++ R/1-model/1-model.R
@@ -1,23 +1,28 @@
 source("R/1-model/1-setup.R")
 
 par <- list(
-  mod = list(
-    mod_null = ppm_par(),
-    mod_manual = ppm_par(half_life = 50,
-                         start = 0.15,
-                         end = 0.0005)
-  ),
   seq_length = 20L, # number of tones
   tone_length = 0.05, # seconds
-  cp_method = "AMOC",
-  cp_penalty = "SIC",
-  cp_threshold = 0.9,
-  cp_burn_in = 30L)
+  ppm = quote(PPMdecay::ppm_options(PPMdecay::decay_buffer(
+    buffer_time = 2,
+    buffer_items = 13,
+    buffer_rate = 0.77,
+    stm_half_life = 2,
+    stm_rate = 0.53,
+    ltm_rate = 0.1,
+    noise = 0.5
+  ), order_bound = 10)),
+  cp = list(
+    method = "Mann-Whitney",
+    t1_error_rate = 10000,
+    startup = 20 
+  )
+)
 
 dat <- readRDS(file = "output/data-01-participants.rds")
 # dat <- dat %>% filter(subj < 4 & trialN < 4 & block == 2) # for testing only
 # dat <- dat %>% filter(subj < 4) # for testing only
-# dat <- dat %>% filter(subj == 1) # for testing only
+dat <- dat %>% filter(subj == 1 & trialN < 4 & block == 1) # for testing only
 
 alphabet <- readRDS(file = "output/alphabet.rds")
 dat <- add_idyom_ic(dat, alphabet, par)
--- R/1-model/functions/ppmdecay.R
+++ R/1-model/functions/ppmdecay.R
@@ -1,57 +1,34 @@
-ppm_par <- function(order_bound = 10L,
-                    shortest_deterministic = FALSE,
-                    exclusion = FALSE,
-                    update_exclusion = FALSE,
-                    escape = "A",
-                    half_life = 50,
-                    start = 1,
-                    end = 1) {
-  x <- as.list(environment())
-  x[order(names(x))]
-}
-
 add_idyom_ic <- function(dat, alphabet, par) {
-  # Check that the data are organised by increasing time
   stopifnot(order(dat$subj, dat$block, dat$trialN) == seq_len(nrow(dat)))
   
-  dat <- group_by(dat, subj)
-  
-  for (i in seq_along(par$mod)) {
-    label <- names(par$mod)[i]
-    dat <- mutate(dat, 
-                  !!label := ic_subj(seq,
-                                     alphabet = !!alphabet,
-                                     par = !!par$mod[[i]][c("order_bound", 
-                                                            "shortest_deterministic",
-                                                            "exclusion",
-                                                            "update_exclusion", 
-                                                            "escape",
-                                                            "half_life",
-                                                            "start",
-                                                            "end")],
-                                     sub = unique(subj)))
-  }
-  dat %>% ungroup()
+  dat %>% 
+    group_by(subj) %>% 
+    mutate(mod = ic_subj(seq, 
+                         alphabet = !!alphabet,
+                         par = !!par,
+                         sub = unique(subj))) %>% 
+    ungroup()
 }
 
 
 ic_subj <- function(seqs, alphabet, par, subj) {
-  stopifnot(is.list(seqs))
-  mod <- new_model(alphabet = alphabet, max_order_bound = par$order_bound)
+  stopifnot(is.list(seqs), length(subj) == 1)
+  
+  ppm_par <- eval(par$ppm)
+  mod <- PPMdecay::new_model(alphabet = alphabet, max_order_bound = ppm_par$order_bound)
   
   message("Performing information_theoretic analyses on subject ", subj, "...")
-  plyr::llply(seqs, function(seq) {
-    predict_seq(mod, seq = seq, options = ppm_options(
-      order_bound = par$order_bound,
-      shortest_deterministic = par$shortest_deterministic,
-      exclusion = par$exclusion,
-      update_exclusion = par$update_exclusion,
-      escape = par$escape, 
-      decay = decay_exp(half_life = par$half_life, 
-                        start = par$start,
-                        end = par$end)
-    ))
-  }, .progress = "text")
+
+  N <- length(seqs)
+  pb <- utils::txtProgressBar(max = N, style = 3)
+  res <- vector(mode = "list", length = N)
+  for (i in seq_len(N)) {
+    res[[i]] <- PPMdecay::predict_seq(mod, seq = seqs[[i]], options = ppm_par)
+    utils::setTxtProgressBar(pb, value = i)
+  }
+  close(pb)
+  
+  res
 }
 
 ic_subj <- memoise::memoise(ic_subj, cache = memoise::cache_filesystem(path = "cache/ic_subj"))
