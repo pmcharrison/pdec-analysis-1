8eb84f1f42603495e9c61511d072052b735c0b12
Peter Harrison <pmc.harrison@gmail.com>
2019-03-14 13:28:36 +0000

    New starting parameters

--- R/1-model/1-model.R
+++ R/1-model/1-model.R
@@ -5,18 +5,18 @@ par <- list(
   tone_length = 0.05, # seconds
   ppm = c(
     buffer_length_time = 2,
-    buffer_length_items = 13,
-    buffer_weight = 0.77,
-    stm_half_life = 2,
-    stm_weight = 0.53,
+    buffer_length_items = 15,
+    buffer_weight = 1.6,
+    stm_half_life = 0.8,
+    stm_weight = 0.45,
     ltm_weight = 0.005,
-    noise = 0.9,
+    noise = 0.5,
     order_bound = 10
   ),
   optim = list(
     which = c("noise", "ltm_weight"),
     lower = c(0, 0),
-    upper = c(10, 0.53),
+    upper = c(10, 0.45),
     control = list(
       maxeval = 50,
       ftol_abs = 0.01
